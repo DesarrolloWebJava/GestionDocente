832e79e185de0623aa48217fd97355c3af42e62f
Curso <Curso@formacion-TOS11.ipartekaula.com>
2017-01-17 13:35:26 +0100

    creamos un servlet

---
+++ gestiondocente/src/main/java/com/ipartek/formacion/controler/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * este paquete va a contener las clases javas propias de la capa controlador.
+ * Estas clases van a ser instancias de Servlet.
+ */
+/**
+ * @author Curso
+ *
+ */
+package com.ipartek.formacion.controler;
