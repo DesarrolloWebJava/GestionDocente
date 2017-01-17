f77b16953ca511358c349d084e17e0464df9bc7d
Curso <Curso@Formacion-TOSH1.ipartekaula.com>
2017-01-17 13:36:05 +0100

    Creado la capa controller Creado un servlet en la capa controller

---
+++ gestiondocente/src/main/java/com/ipartek/formacion/controller/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Este paquete va a contener las clases java propias de la capa controlador.
+ * Todas las clases van a ser instancias de Servlet.
+ */
+/**
+ * @author Curso
+ *
+ */
+package com.ipartek.formacion.controller;
