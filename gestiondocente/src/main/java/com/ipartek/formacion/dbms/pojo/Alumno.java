3dae30dbf1e5caa437a0337164a10b9463afb4b7
Curso <Curso@Formacion-TOS12.ipartekaula.com>
2017-01-17 10:04:07 +0100

    Añadido los comentarios a la clase Alumno

--- gestiondocente/src/main/java/com/ipartek/formacion/dbms/pojo/Alumno.java
+++ gestiondocente/src/main/java/com/ipartek/formacion/dbms/pojo/Alumno.java
@@ -1,3 +1,4 @@
+/* paquete donde se guardan las entidades. */
 package com.ipartek.formacion.dbms.pojo;
 
 import java.io.Serializable;
@@ -5,80 +6,109 @@ import java.io.Serializable;
 /**
  * 
  * @author Urko Villanueva
+ * @author Raúl de Roba 17/01/17 (Añadido de comentarios.)
+ * 
+ * Clase de la entidad Alumno (Heredada de la clase persona).
  *
  */
 public class Alumno extends Persona implements Comparable<Alumno>, Serializable {
 
-	/**
-	 * 
-	 */
+	/* Constante serial de la clase.*/
 	private static final long serialVersionUID = -6698866485450376235L;
+	/* Constate para valores nulos. Se asigna -1 asumiendo que no se ha introducido 
+	 * valor reseñable.ningún*/
 	public static final int CODIGO_NULO = -1;
+	/* Atributo identificativo de la 'Alumno'. */
 	private int codigo;
+	/* Atributo //TODO */
 	private boolean activo;
+	/* Atributo nº de Hermanos de la 'Alumno'.*/
 	private int nHermanos;
 
+	/* Constructor sin parametros. */ 
 	public Alumno() {
-		super();// constructor de la clase padre
+		/* Se llama al constructor padre.*/
+		super();
+		/* Se asigna el valor nulo. (Recogido de la constante de clase.)*/
 		this.codigo = CODIGO_NULO;
+		/* Se activa el 'Alumno'. */
 		this.activo = true;
+		/* Se inicializa el numero de hermanos a 0.*/
 		this.nHermanos = 0;
 
 	}
 
+	/* Getter de codigo.*/
 	public int getCodigo() {
+		/* Se devuelve el 'codigo' de la 'Alumno'.*/
 		return codigo;
 	}
 
+	/* Setter del codigo. */
 	public void setCodigo(int codigo) {
+		/* Se asigna el 'codigo' a la clase.*/
 		this.codigo = codigo;
 	}
 
 	@Override
+	/* Metodo que devuelve una versión (personalizada) de la clase como String.*/
 	public String toString() {
-		return this.getCodigo() + " " + this.getApellidos() + ", " + this.getNombre() + " " + this.getDni();
+		/* Se devuelve un String con el codigo, apellido , el nombre y D.N.I.*/
+		return this.getCodigo() + " " + this.getApellidos() + ", " + 
+			   this.getNombre() + " " + this.getDni();
 	}
 
+	/**
+	 * @return activo
+	 * Metodo que devuelve si la persona está activa.
+	 */
 	public boolean isActivo() {
+		/* Se devuelve el atributo 'activo' de la 'Alumno'.*/
 		return activo;
 	}
 
+	/* Setter del atributo activo. */	
 	public void setActivo(boolean activo) {
+		/* Se asigna el atributo 'activo' de la clase.*/
 		this.activo = activo;
 	}
 
+	/* getter del nº de hermanos. */
 	public int getnHermanos() {
+		/* Se devuelve el atributo 'nhermano' de la clase.*/
 		return nHermanos;
 	}
 
+	/* setter del nº de hermanos. */
 	public void setnHermanos(int nHermanos) {
+		/* Se asigna el atributo 'nhermanos' de la clase.*/
 		this.nHermanos = nHermanos;
 	}
 
-	/**
-	 * Se usa en el caso de ordenamiento de List o Array
-	 * 
-	 * @param o
-	 * @return
-	 */
+	/* Metodo que es requerido en el ordenamiento de List o Array. */
 	@Override
 	public int compareTo(Alumno o) {
+		/* Devuelve la comparación entre el objeto pasado por parametro
+		 * y la propia clase.
+		 * Devolverá -1 si es menor,0 si es igual o 1 si es mayor.*/
 		return this.getApellidos().compareToIgnoreCase(o.getApellidos());
 	}
 
-	/**
-	 * Para evaluar si los objetos son iguales
-	 */
-
+	/* Metodo que compara la igualdad de la clase y un objeto pasado por parametro. */
 	@Override
 	public boolean equals(Object obj) {
+		/* Se inicializa el reultado a 'falso'.*/
 		boolean iguales = false;
+		/* Se comprueba que el objeto pasado por parametro sea un 'Alumno'*/
 		if (obj instanceof Alumno) {
-			Alumno alum = (Alumno) obj;
-			if (this.codigo == alum.getCodigo()) {
+			/* Se compara los codigos del objeto pasado por parametro y 
+			 * la clase actual.*/
+			if (this.codigo == ((Alumno) obj).getCodigo()) {
+				/* Se asigna un 'true' al resultado.*/
 				iguales = true;
 			}
 		}
+		/* Se devuelve el resultado.*/
 		return iguales;
 	}
 
