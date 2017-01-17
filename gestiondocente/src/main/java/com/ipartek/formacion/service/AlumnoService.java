18010871d3e493d749e48bbf5e7c454bf2a4dc7a
Curso <Curso@Formacion-TOSH6.ipartekaula.com>
2017-01-17 11:18:08 +0100

    Cambiar los archivos de inicio de la aplicación. Importar la capa Service de profesor y de alumno. Creada la interfaz de alumno. Subo todos los nuevos archivos con Select All, y hacemos commit&push.

---
+++ gestiondocente/src/main/java/com/ipartek/formacion/service/AlumnoService.java
@@ -0,0 +1,26 @@
+package com.ipartek.formacion.service;
+
+import java.util.List;
+
+import com.ipartek.formacion.dbms.pojo.Alumno;
+
+/**
+ * 
+ * @author Curso
+ *
+ */
+public interface AlumnoService {
+	
+	//Add CRUD
+	
+	public Alumno create(Alumno alumno);
+	
+	public Alumno getById(int codigo);
+	
+	public List<Alumno> getAll();
+	
+	public Alumno update(Alumno alumno);
+	
+	public void delete(int codigo);
+
+}
