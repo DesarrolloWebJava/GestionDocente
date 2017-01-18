4a06b26cd229eba73813568fc70c056a9f56532f
Raúl de Roba <fnx.sistemas@gmail.com>
2017-01-18 10:06:01 +0100

    Añadido comentarios a las clases Util y AlumnoServiceImpException.

--- gestiondocente/src/main/java/com/ipartek/formacion/service/Util.java
+++ gestiondocente/src/main/java/com/ipartek/formacion/service/Util.java
@@ -1,11 +1,22 @@
+/* Paquete donde se guardan las clase de las operaciones con entidades. */
 package com.ipartek.formacion.service;
 
+/** 
+ * @author Urko Villanueva
+ * @author Raúl de Roba 18/01/17 (Añadido de comentarios.)
+ * 
+ *  <p> * Esta clase se va encargar de gestionar las operaciones generales de las distintas 
+ *        clases del paquete 'Servcice'.</p> * 
+ */ 
 public class Util {
-
+	
+	/* Constructor sin parametros de la clase util.*/
 	private Util() {
 	}
 
+	/* Metodo que valida un D.N.I.*/
 	public static boolean validarDni(String dni) {
+		/* Se devuelve siempre 'True'.*/
 		return true;
 	}
 }
--- gestiondocente/src/main/java/com/ipartek/formacion/service/exceptions/AlumnoServiceImpException.java
+++ gestiondocente/src/main/java/com/ipartek/formacion/service/exceptions/AlumnoServiceImpException.java
@@ -1,29 +1,50 @@
+/* Paquete de excepciones de las clases del paquete Service.*/
 package com.ipartek.formacion.service.exceptions;
+
+/**
+ * 
+ * @author Raúl de Roba 18/01/17.
+ * 
+ * <p>Clase que gestiona las excepciones de la clase Alumno.</p>
+ *
+ */
 
 public class AlumnoServiceImpException extends Exception {
 
-	/**
-	 * 
-	 */
+	/* Constante serial de la clase.*/
 	private static final long serialVersionUID = 1L;
+	/* Se declaran las constantes de codigo de error para la excepción. */
 	public static final int COD_ALUMNO_NO_ENCONTRADO = 500;
-	public static final String MSG_ALUMNO_NO_ENCONTRADO = "No se encuentra el alumno solicitado";
+	/* Se declaran las constantes de mensajes de error para la excepción. */
+	public static final String MSG_ALUMNO_NO_ENCONTRADO = 
+			                                       "No se encuentra el alumno solicitado";
 
+	/* Atributo 'codigo' con el que trabajar el codigo de error.*/
 	private int codigo;
+	/* Atributo 'mensaje' de error.*/
 	private String mensaje;
-
-	public AlumnoServiceImpException(String message) {
+	
+	/* Constructor con el mensaje de error como parametro. */
+	public AlumnoServiceImpException(String message) {		
+		/* Se llama al constructor de la clase pasandole el mensaje de error.*/
 		super(message);
+		/* Se asigna el mensaje de error de la clase.*/
 		this.mensaje = message;
 	}
 
+	/* Constructor con el codigo y mensaje de error como parametro. */
 	public AlumnoServiceImpException(int codigo, String mensaje) {
+		/* Se llama al constructor de la clase pasandole el mensaje de error.*/
 		super(mensaje);
+		/* Se asigna el codigo de error de la clase.*/
 		this.codigo = codigo;
+		/* Se asigna el mensaje de error de la clase.*/
 		this.mensaje = mensaje;
 	}
 
+	/* Getter del atributo 'codigo'.*/
 	public int getCodigo() {
+		/* Se devuelve el codigo.*/
 		return codigo;
 	}
 
