2de6d4538f3305ebd17d2d910fb160b3cb2a1c55
Raul <fnx.sistemas@gmail.com>
2017-01-19 13:28:51 +0100

    Actualizado Servlet Alumno para la gestión de CRUD

--- gestiondocente/src/main/java/com/ipartek/formacion/controller/AlumnoServet.java
+++ gestiondocente/src/main/java/com/ipartek/formacion/controller/AlumnoServet.java
@@ -3,6 +3,7 @@
 package com.ipartek.formacion.controller;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import javax.servlet.RequestDispatcher;
@@ -12,6 +13,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import com.ipartek.formacion.dbms.pojo.Alumno;
+import com.ipartek.formacion.dbms.pojo.exceptions.PersonaException;
 import com.ipartek.formacion.service.AlumnoService;
 import com.ipartek.formacion.service.AlumnoServiceImp;
 
@@ -62,12 +64,14 @@ public class AlumnoServet extends HttpServlet {
 				/* Se comprueba si se ha recibido la operación de crear.*/
 				case Constantes.OP_CREATE :
 					/* Se redirecciona a la url del formulario del alumno.*/
+					// TODO Constante alumno.jsp
 					rd = req.getRequestDispatcher("alumnos/alumno.jsp");
 					/* Se sale de la estructura 'Swicth'*/
 					break;
 				/* Se comprueba si se ha recibido la operación de modificar.*/
 				case Constantes.OP_UPDATE :
 					/* Se redirecciona a la url del formulario del alumno.*/
+					// TODO Constante alumno.jsp
 					rd = req.getRequestDispatcher("alumnos/alumno.jsp");
 					/* Se sale de la estructura 'Swicth'*/
 					break;
@@ -83,6 +87,7 @@ public class AlumnoServet extends HttpServlet {
 				cargarListaAlumnos(req);
 				break;
 			}
+		/* Se captura la excepción.*/	
 		}catch (Exception e){
 			/* En caso que la operacion recibida no sea entero,
 			 * se llama al manda a la página inicial. */
@@ -109,9 +114,89 @@ public class AlumnoServet extends HttpServlet {
 	}
 
 	/* Metodo a ejecutar al recibir una petición Post. */
-	protected void doPost(HttpServletRequest req, HttpServletResponse res) 
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) 
 			                                        throws ServletException, IOException {
+		/* Se declara y se instancia el objeto donde trabajar con el alumno.*/
+		Alumno alumno = new Alumno();
+		/* Se monta estructura para la captura de excepciones.*/
+		try {
+			/* Se llama al metodo que devuelve el alumno 
+			 * pasado por los parametros de la request.*/
+			alumno =recogerParametros(req);
+			/* Se declara la variable para transmitir el mensaje auditoria 
+			 * de la operacion.*/
+			String mensaje ="";
+			/* Se comprueba si se pretende añadir alumno o modificarlo en función si
+			 * el codigo del alumno es mayor que la cosntante de 
+			 * codigo nulo(codigo inicial). */
+			if(alumno.getCodigo()>alumno.CODIGO_NULO){
+				/* Se actualiza el alumno.*/
+				aS.update(alumno);
+				/* Se asigna el mensaje de actualización satisfactoria.*/
+				mensaje = "El alumno ha sido actualizado correctamente.";
+			} else{
+				/* Se crea el alumno.*/
+				aS.create(alumno);
+				/* Se asigna el mensaje de creación satisfactoria.*/
+				mensaje = "El alumno ha sido creado correctamente.";				
+			}
 		
+		/* Se captura la excepción.*/	
+		} catch (Exception e) {
+			/* Se asigna la url del formulario 'alumno.jsp' 
+			 * para redireccionar tras el error.*/
+			// TODO Constante alumno.jsp
+			rd = req.getRequestDispatcher("alumnos/alumno.jsp");
+			/* Se asigna el atributo del error del mensaje para pasarlo al request.*/
+			req.setAttribute("mensaje",e.getMessage());
+		}
+		/* Se redirije a la url que se haya indicado.*/
+		rd.forward(req, resp);
+		
+	}
+
+	/* Metodo que devuelve el alumno pasado por los parametros de la request.*/
+	private Alumno recogerParametros(HttpServletRequest req) throws Exception {
+		/* Se declara y se instancia el objeto donde trabajar con el alumno.*/
+		Alumno alumno = new Alumno();
+		/* Se monta estructura para la captura de excepciones.*/
+		try{
+			/* Se declara y recoge el codigo recogido de la request.*/
+			int codigo = Integer.parseInt(req.getParameter(Constantes.PAR_CODIGO));
+			/* Se asigna el codigo al alumno.*/
+			alumno.setCodigo(codigo);
+			/* Se asigna los apellidos al alumno con los apellidos 
+			 * recogidos por parametro.*/
+			alumno.setApellidos(req.getParameter(Constantes.PAR_APELLIDOS));				
+			/* Se asigna la fecha de nacimiento al alumno con los fecha de nacimiento 
+			 * recogidos por parametro.*/
+			String date = req.getParameter(Constantes.PAR_FNACIMIENTO);
+			String pattern = "dd/MM/yyyy";
+			SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+			alumno.setfNacimiento(dateFormat.parse(date));
+			/* Se asigna los email al alumno con los email 
+			 * recogidos por parametro.*/
+			alumno.setEmail(req.getParameter(Constantes.PAR_EMAIL));
+			/* Se asigna el numero de hermanos al alumno con el numero de hermanos 
+			 * recogidos por parametro.*/
+			alumno.setnHermanos(
+					       Integer.parseInt(req.getParameter(Constantes.PAR_NHERMANOS)));
+			/* Se asigna el estado activo al alumno con el estado activo
+			 * recogidos por parametro.*/
+			alumno.setActivo(
+					      Boolean.parseBoolean(req.getParameter(Constantes.PAR_ACTIVO)));			
+			
+		/* Se captura la excepción de tipo persona.*/	
+		}catch (PersonaException e){
+			/* Se trasfiere la excepcion a la clase padre.*/
+			throw new PersonaException(e.getMessage());
+		/* Se captura la excepción.*/	
+		}catch (Exception e){
+			/* Se trasfiere la excepcion a la clase padre.*/
+			throw new Exception(e.getMessage());		
+		}
+		/* Se devuelve el alumno creado.*/
+		return alumno;
 	}
 
 }
