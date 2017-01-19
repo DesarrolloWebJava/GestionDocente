b737f70dc10893449e57dcb7a1a8a8a8ed339a87
Enrique J. Ruiz <enriquejruizj@gmail.com>
2017-01-19 13:36:48 +0100

    Crear alumno con excepciones

--- gestiondocente/src/main/java/com/ipartek/formacion/controller/AlumnoServlet.java
+++ gestiondocente/src/main/java/com/ipartek/formacion/controller/AlumnoServlet.java
@@ -1,13 +1,17 @@
 package com.ipartek.formacion.controller;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.logging.SimpleFormatter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import org.omg.CORBA.Request;
 
 import com.ipartek.formacion.dbms.pojo.Alumno;
 import com.ipartek.formacion.service.AlumnoService;
@@ -70,7 +74,47 @@ public class AlumnoServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		Alumno alumno = null; 
+		String mensaje="";
+		try{
+			alumno = recogerParametros(req);
+			
+			if(alumno.getCodigo()>Alumno.CODIGO_NULO){
+				aS.update(alumno);
+				mensaje = "El alumno a sido actializado correctamente";
+			}else{
+				aS.create(alumno);
+				mensaje = "El alumno a sido creado correctamente";
+			}
+		} catch (Exception e){
+			rd = req.getRequestDispatcher(Constantes.JSP_CREAR_ALUMNOS);
+			mensaje= e.getMessage();
+		}
+		req.setAttribute("mensaje", mensaje);
+		rd.forward(req, resp);
 		
+	}
+	private Alumno recogerParametros(HttpServletRequest req) throws Exception {
+		Alumno alumno = new Alumno();
+		try{
+			alumno.setCodigo(Integer.parseInt(req.getParameter(Constantes.PAR_CODIGO)));
+			alumno.setNombre(req.getParameter(Constantes.PAR_NOMBRE));
+			alumno.setApellidos(req.getParameter(Constantes.PAR_APELLIDOS));
+			alumno.setDni(req.getParameter(Constantes.PAR_DNI));
+			alumno.setEmail(req.getParameter(Constantes.PAR_EMAIL));
+			alumno.setDireccion(req.getParameter(Constantes.PAR_DIRECCION));
+			alumno.setnHermanos(Integer.parseInt(req.getParameter(Constantes.PAR_NHERMANOS)));
+			alumno.setActivo(Boolean.parseBoolean(req.getParameter(Constantes.PAR_ACTIVO)));
+			String date = req.getParameter(Constantes.PAR_FNACIMIENTO); 
+			String pattern ="dd/MM/yyyy";
+			SimpleDateFormat dateFormat = new SimpleDateFormat(pattern);
+			alumno.setfNacimiento(dateFormat.parse(date));
+			
+		} catch(Exception e){
+			throw new Exception("Los datos son incorrectos");
+		}
+		
+		return alumno;
 	}
 	@Override
 	public void destroy() {
