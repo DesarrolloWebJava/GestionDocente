5e01683b719988aecf5f1f7c3dfcf131df11b8f1
urko villanueva <urko.informatica@gmail.com>
2017-01-30 11:16:00 +0100

    LOG configurado

---
+++ gestiondocente/src/main/java/com/ipartek/formacion/controller/listeners/InitListener.java
@@ -0,0 +1,77 @@
+package com.ipartek.formacion.controller.listeners;
+
+import java.util.Properties;
+
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
+/**
+ * Application Lifecycle Listener implementation class InitListener
+ *
+ */
+public class InitListener implements ServletContextListener, ServletContextAttributeListener {
+
+	private static final String NFICHERO = "log4j.properties";
+	private static final Logger LOG = Logger.getLogger(InitListener.class);
+
+	/**
+	 * Default constructor.
+	 */
+	public InitListener() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see ServletContextAttributeListener#attributeAdded(ServletContextAttributeEvent)
+	 */
+	public void attributeAdded(ServletContextAttributeEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see ServletContextAttributeListener#attributeRemoved(ServletContextAttributeEvent)
+	 */
+	public void attributeRemoved(ServletContextAttributeEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see ServletContextListener#contextDestroyed(ServletContextEvent)
+	 */
+	public void contextDestroyed(ServletContextEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see ServletContextAttributeListener#attributeReplaced(ServletContextAttributeEvent)
+	 */
+	public void attributeReplaced(ServletContextAttributeEvent arg0) {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see ServletContextListener#contextInitialized(ServletContextEvent)
+	 */
+	public void contextInitialized(ServletContextEvent arg0) {
+		loadLog4j(arg0);
+	}
+
+	private void loadLog4j(ServletContextEvent arg0) {
+		Properties props = new Properties();
+		try {
+			// props.load(this.getClass().getClassLoader().getResource());
+			PropertyConfigurator.configure(this.getClass().getClassLoader().getResource(NFICHERO));
+			LOG.trace("LOG cargado");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
